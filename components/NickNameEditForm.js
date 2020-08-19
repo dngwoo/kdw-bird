301396f80654779997ae455e383e432fac9d38e0
kddng <akfmzk84@naver.com>
2020-08-20 01:25:57 +0900

    NickNameEditForm proptypes 삭제

--- components/NickNameEditForm.js
+++ components/NickNameEditForm.js
@@ -1,5 +1,4 @@
 import React, { useMemo } from "react";
-import PropTypes from "prop-types";
 import { Form, Input } from "antd";
 
 const NickNameEditForm = () => {
@@ -16,11 +15,6 @@ const NickNameEditForm = () => {
       <Input.Search addonBefore="닉네임" enterButton></Input.Search>
     </Form>
   );
-};
-
-NickNameEditForm.propTypes = {
-  header: PropTypes.string.isRequired,
-  data: PropTypes.array.isRequired,
 };
 
 export default NickNameEditForm;
