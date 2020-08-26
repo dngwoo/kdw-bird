f2c3fd77c785e571c66e101c715a5c6a934f2521
kddng <akfmzk84@naver.com>
2020-08-26 23:41:43 +0900

    리덕스사가 쪼개기

---
+++ sagas/post.js
@@ -0,0 +1,31 @@
+import { call, fork, takeLatest, delay, put, all } from "redux-saga/effects";
+import axios from "axios";
+
+// addPost
+// function addPostAPI(data) {
+//   return axios.post("/api/post", data);
+// }
+
+function* addPost(action) {
+  yield delay(1000);
+  try {
+    // const result = yield call(addPostAPI, action.data);
+    yield put({
+      type: "ADD_POST_SUCCESS",
+      //   data: result.data,
+    });
+  } catch (error) {
+    yield put({
+      type: "ADD_POST_FAILURE",
+      data: error.response.data,
+    });
+  }
+}
+
+function* watchAddPost() {
+  yield takeLatest("ADD_POST_REQUEST", addPost);
+}
+
+export default function* postSaga() {
+  yield all[fork(watchAddPost)];
+}
---
+++ sagas/user.js
@@ -0,0 +1,56 @@
+import { call, fork, takeLatest, delay, put, all } from "redux-saga/effects";
+import axios from "axios";
+
+// login
+// function logInAPI(data) {
+//   return axios.post("/api/login", data);
+// }
+
+function* logIn(action) {
+  yield delay(1000);
+  try {
+    // const result = yield call(logInAPI, action.data);
+    yield put({
+      type: "LOG_IN_SUCCESS",
+      //   data: result.data,
+    });
+  } catch (error) {
+    yield put({
+      type: "LOG_IN_FAILURE",
+      data: error.response.data,
+    });
+  }
+}
+
+function* watchLogin() {
+  yield takeLatest("LOG_IN_REQUEST", logIn);
+}
+
+// logout
+// function logOutAPI() {
+//   return axios.post("/api/logout");
+// }
+
+function* logOut() {
+  yield delay(1000);
+  try {
+    // const result = yield call(logOutAPI);
+    yield put({
+      type: "LOG_OUT_SUCCESS",
+      //   data: result.data,
+    });
+  } catch (error) {
+    yield put({
+      type: "LOG_OUT_FAILURE",
+      data: error.response.data,
+    });
+  }
+}
+
+function* watchLogOut() {
+  yield takeLatest("LOG_OUT_REQUEST", logOut);
+}
+
+export default function* userSaga() {
+  yield all([fork(watchLogin), fork(watchLogOut)]);
+}
